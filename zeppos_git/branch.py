59ef07fe520b77948bdc7cc32c243b9522fec70d
Haegdorens, Gert <gert.haegdorens@yrcw.com>
2021-02-10 11:18:27 -0600

    Add caching

--- zeppos_git/branch.py
+++ zeppos_git/branch.py
@@ -1,14 +1,15 @@
 import git
 from zeppos_root.root import Root
 from zeppos_logging.app_logger import AppLogger
-
+from cachetools import cached, TTLCache
 
 class Branch:
     @staticmethod
+    @cached(cache=TTLCache(maxsize=1024, ttl=600))
     def get_current():
         g = git.cmd.Git(Root.find_root_of_project(__file__))
         for line in g.branch().split('\n'):
             if line.startswith("*"):
-                AppLogger.logger.info(f"Current Git Branch: {line[1:].strip()}")
+                AppLogger.logger.debug(f"Current Git Branch: {line[1:].strip()}")
                 return line[1:].strip()
         return None
